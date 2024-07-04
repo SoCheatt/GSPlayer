08bd814819228017fd5cb5e5aa08975b818efac7
Socheat <momsocheat60@gmail.com>
2024-07-04 14:28:52 +0700

    fix expiration cache file

--- GSPlayer/Classes/Cache/VideoCacheManager.swift
+++ GSPlayer/Classes/Cache/VideoCacheManager.swift
@@ -137,3 +137,36 @@ extension Date {
         return localDate
     }
 }
+
+
+struct TimeConstants {
+    static let secondsInOneDay = 86_400
+}
+
+public enum GSExpiration {
+    /// The item never expires.
+    case never
+    /// The item expires after a time duration of given seconds from now.
+    case seconds(TimeInterval)
+    /// The item expires after a time duration of given days from now.
+    case days(Int)
+    /// The item expires after a given date.
+    case date(Date)
+    /// Indicates the item is already expired. Use this to skip cache.
+    case expired
+    
+    public func estimatedExpirationSince(_ date: Date) -> Date {
+        switch self {
+        case .never: return .distantFuture
+        case .seconds(let seconds):
+            return date.addingTimeInterval(seconds)
+        case .days(let days):
+            let duration: TimeInterval = TimeInterval(TimeConstants.secondsInOneDay) * TimeInterval(days)
+            return date.addingTimeInterval(duration)
+        case .date(let ref):
+            return ref
+        case .expired:
+            return .distantPast
+        }
+    }
+}
